1904d1441ea6105bbe63030671382b4932006965
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-03-18 13:07:29 -0400

    creating skel

--- secret.sh
+++ secret.sh
@@ -4,6 +4,6 @@
 kubectl create secret docker-registry dockercred \
     --namespace kaniko \
     --docker-server=https://index.docker.io/v1/ \
-    --docker-username=robinhoodis \
-    --docker-password=Kosdir-kykruh-vekwo8\
-    --docker-email=robin@mordasiewicz.com
+    --docker-username=<username> \
+    --docker-password=<password>\
+    --docker-email=<email>
