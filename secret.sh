eed5526fb62d867a907671ee1b742f711aa144e8
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-03-18 13:09:26 -0400

    Delete secret.sh

--- secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-kubectl create secret docker-registry dockercred \
-    --namespace kaniko \
-    --docker-server=https://index.docker.io/v1/ \
-    --docker-username=<username> \
-    --docker-password=<password>\
-    --docker-email=<email>
